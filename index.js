70381fca7f4cb861ebb56fac6e4426dea6dbedaf
Varsha yadav <148608112+VershaRani-9917@users.noreply.github.com>
2025-02-12 09:17:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// SHORTHAND
+
+let personName = "Maze";
+let age = 20;
+
+// const obj = {
+//   personName: personName,
+//   age: age,
+// };
+// const obj1 = { personName };
+
+const createPerson = (personName, age) => {
+  return { personName, age };
+};
+console.log(createPerson(personName, age));
